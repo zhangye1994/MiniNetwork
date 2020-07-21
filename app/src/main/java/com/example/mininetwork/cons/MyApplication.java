36f88434752f988ba33707260eb17a9907818fbe
残魂星梦 <2893556802@qq.com>
2020-07-22 01:26:37 +0800

    1.加了点注释，使用的时候请关注一下，懒的程序员日记 2.以后会做更多的优化，敬请期待

--- app/src/main/java/com/example/mininetwork/cons/MyApplication.java
+++ app/src/main/java/com/example/mininetwork/cons/MyApplication.java
@@ -13,6 +13,9 @@ public class MyApplication extends Application {
         api = RetrofitHelper.create(Api.class);
     }
 
+    /***有一个猜想哈，这么写可能会造成内存泄漏，因为App并没有被关闭，
+     * 所以对象可能不会被回收，如果说静态api变量没有导致使用问题的话
+     * 不推荐大家这样调用方法或者去写这样的方法*/
     public static Api getApi(){
         return RetrofitHelper.create(Api.class);
     }
